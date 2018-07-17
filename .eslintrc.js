3d61135441414d0645ff06df9021080961aa26c9
Yoon <yty3592@gmail.com>
2018-07-17 20:26:24 +0900

    .eslintrc 설정

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,7 @@ module.exports = {
     "env": {
         "browser": true,
         "es6": true,
-        "amd": true
+        "amd": true  // require 사용
     },
     "extends": [
         "eslint:recommended", "plugin:react/recommended"
@@ -17,10 +17,10 @@ module.exports = {
     },
     "plugins": ["react"],
     "rules": {
-        "arrow-body-style": 'off', // arrow body 쓸거임
-        "indent": ["error", 4],
+        "arrow-body-style": 'off', // arrow body 쓸수있도록 
+        "indent": ["error", 4], // 들여쓰기
         "linebreak-style": ["error", "unix"],
-        "quotes": ["off", "single", "double"],
-        "semi": ["error", "always"]
+        "quotes": ["off", "single", "double"], // 콤마 관련에러 끄기
+        "semi": ["error", "always"] // 세미콜론 안붙이면 에러
     }
 };
